ee2cf4d18db4988638430bff7309a78b15e1f25a
Yuki Roppongi <az0.mint9@gmail.com>
2019-01-28 20:58:53 +0900

    Specificationの実装を変更

--- src/main/java/jp/personal/gi/java/bases/ddd/frame/Specification.java
+++ src/main/java/jp/personal/gi/java/bases/ddd/frame/Specification.java
@@ -4,43 +4,27 @@ import java.util.Optional;
 import java.util.function.Predicate;
 
 @FunctionalInterface
-public interface Specification<T> {
-    boolean isSatisfiedBy(T instance);
+public interface Specification<T> extends Predicate<T> {
+    @Override
+    boolean test(T target);
 
-    default boolean notSatisfiedBy(T instance) {
-        return !isSatisfiedBy(instance);
+    @Override
+    default Specification<T> and(Predicate<? super T> other) {
+        return target -> this.test(target) && other.test(target);
     }
 
-    default Specification<T> not() {
-        return this::notSatisfiedBy;
+    @Override
+    default Specification<T> or(Predicate<? super T> other) {
+        return target -> this.test(target) || other.test(target);
     }
 
-    default Specification<T> and(Specification<T> otherSpecification) {
-        return instance -> this.isSatisfiedBy(instance)
-                && otherSpecification.isSatisfiedBy(instance);
-    }
-
-    default Specification<T> andNot(Specification<T> otherSpecification) {
-        return instance -> this.isSatisfiedBy(instance)
-                && otherSpecification.notSatisfiedBy(instance);
-    }
-
-    default Specification<T> or(Specification<T> otherSpecification) {
-        return instance -> this.isSatisfiedBy(instance)
-                || otherSpecification.isSatisfiedBy(instance);
-    }
-
-    default Specification<T> orNot(Specification<T> otherSpecification) {
-        return instance -> this.isSatisfiedBy(instance)
-                || otherSpecification.notSatisfiedBy(instance);
-    }
-
-    default Predicate<T> toPredicate() {
-        return this::isSatisfiedBy;
+    @Override
+    default Specification<T> negate() {
+        return target -> !test(target);
     }
 
     default Optional<T> satisfy(T instance) {
-        return this.isSatisfiedBy(instance)
+        return this.test(instance)
                 ? Optional.of(instance)
                 : Optional.empty();
     }
